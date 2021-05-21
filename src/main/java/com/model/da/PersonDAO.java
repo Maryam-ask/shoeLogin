909435685bb18b6022fdea2a59f3c30c68e4bfc3
Maryam-ask <maryamaskari19@gmail.com>
2021-05-21 15:03:27 +0200

    making PersonDAO interface

---
+++ src/main/java/com/model/da/PersonDAO.java
@@ -0,0 +1,14 @@
+package com.model.da;
+
+import com.model.to.Person;
+
+/**
+ * Created by Maryam Askari
+ * Date: 5/21/2021
+ * Time: 3:01 PM
+ * Project: IntelliJ IDEA
+ */
+public interface PersonDAO {
+    public Person selectValidPerson(String userName, String password);
+    public void insertRegisteredPerson(Person person);
+}
