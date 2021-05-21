ec6da5c000c5d0831fb4c1fef94e2e38060520e1
Maryam-ask <maryamaskari19@gmail.com>
2021-05-21 14:39:18 +0200

    JavaDock for method shaHashing

--- src/main/java/sample/PasswordSecurity.java
+++ src/main/java/sample/PasswordSecurity.java
@@ -10,6 +10,11 @@ import org.apache.commons.codec.digest.DigestUtils;
  */
 public class PasswordSecurity {
 
+    /**
+     * A method for hashing password.
+     * @param password
+     * @return string a hashcode av 256 length
+     */
     public static String shaHashing(String password) {
         String shaHashing = DigestUtils.sha256Hex(password);
         return  shaHashing;
