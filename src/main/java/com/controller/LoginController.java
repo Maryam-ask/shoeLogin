c48589c4e142e91f570165676cc0282f243692d8
Maryam-ask <maryamaskari19@gmail.com>
2021-05-28 22:26:11 +0200

    javaDoc inserted

--- src/main/java/com/controller/LoginController.java
+++ src/main/java/com/controller/LoginController.java
@@ -1,11 +1,13 @@
 package com.controller;
 
+import com.model.da.PersonDAO;
 import com.model.da.PersonDAOImpl;
 import com.model.to.Person;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -15,18 +17,17 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-import com.model.da.DatabaseConnection;
 import sample.Main;
 import sample.PasswordSecurity;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
+
+    private Main main;
 
     @FXML
     private Button cancelButton;
@@ -41,25 +42,34 @@ public class LoginController implements Initializable {
     @FXML
     private PasswordField enterPasswordField;
 
+    private Person person ;
+
 
     public void cancelButtonOnAction(ActionEvent event) { // the cancel button and its action
         Stage stage = (Stage) cancelButton.getScene().getWindow();
         stage.close();
     }
 
-    public void loginButtonOnAction(ActionEvent event) { // the Login Button and its action
-        String userName = usernameTextField.getText();
-        String password = PasswordSecurity.shaHashing(enterPasswordField.getText());
+    public void loginButtonOnAction(ActionEvent event) throws IOException { // the Login Button and its action
 
         if (!usernameTextField.getText().isBlank() && !enterPasswordField.getText().isBlank()) {
-            //validateLogin();
-            PersonDAOImpl personDAO = new PersonDAOImpl();
-            Person person = personDAO.selectValidPerson(userName, password);
-            Main main = new Main();
-            main.shoeTableView();
 
-            if (person.getUserName().equals(userName) && person.getPassword().equals(password)){
+            String userName = usernameTextField.getText();
+            String password = PasswordSecurity.shaHashing(enterPasswordField.getText());
+
+            // validateLogin();
+            PersonDAOImpl personDAO = new PersonDAOImpl();
+            person = personDAO.selectValidPerson(userName, password);
+
+            if (person !=null){
+
                 loginMessageLabel.setText("congratulation!");
+                // Go to Shoes Table Page
+
+                //Main.getStage().getScene().setUserData(person);
+                getMain().shoeTableView(person);
+
+
             }else {
                 loginMessageLabel.setText("Invalid login. Please try again");
             }
@@ -70,7 +80,7 @@ public class LoginController implements Initializable {
     }
 
     public void registerButtonOnAction(ActionEvent event) {
-        creatAccountForm();
+        main.registerStage();;
     }
 
     @Override
@@ -83,6 +93,39 @@ public class LoginController implements Initializable {
         Image lockImage = new Image(lockFile.toURI().toString());
         lockImageView.setImage(lockImage);
     }
+
+
+    /**
+     * When this method called, it will pass the selected person object to
+     * the detailed view
+     * @param event
+     * @throws IOException
+     */
+    public void sendPersonInfoToNewScene(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("resource\\shoesTable.fxml")); // loader will look at this file
+        Parent loginViewParent = loader.load();
+        Scene loginViewScene = new Scene(loginViewParent);
+
+        // Access the controller and call a method.
+        //ShoesTableController controller = loader.getController(); // access the controller in that file
+        //controller.initData(getPerson());
+
+        Stage window =(Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(loginViewScene);
+        window.show();
+
+
+    }
+
+    public Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
 
     /*public void validateLogin() { // To connect to my DB
 
@@ -115,11 +158,21 @@ public class LoginController implements Initializable {
 
      */
 
+    /*public boolean validateLogin(String username, String password){
+        PersonDAO personDAO = new PersonDAOImpl();
+        person = personDAO.selectValidPerson(username,password);
+        if (username.equals(person.getUserName()) && password.equals(person.getPassword()))
+            return true;
+        return false;
 
-    public void creatAccountForm() {
-        Main main = new Main();
-        main.registerStage();
+    }*/
+
+
+    public Person getPerson() {
+        return person;
     }
 
-
+    public void setPerson(Person person) {
+        this.person = person;
+    }
 }
