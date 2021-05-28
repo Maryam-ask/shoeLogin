0cb2d9f7ad536c5c8742631ab8052d60171cb2a6
Maryam-ask <maryamaskari19@gmail.com>
2021-05-28 22:26:47 +0200

    javaDoc inserted

--- src/main/java/com/controller/RegisterController.java
+++ src/main/java/com/controller/RegisterController.java
@@ -3,27 +3,20 @@ package com.controller;
 import com.model.da.PersonDAO;
 import com.model.da.PersonDAOImpl;
 import com.model.to.Person;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
-import com.model.da.DatabaseConnection;
 import sample.Main;
 import sample.PasswordSecurity;
 
 import java.io.File;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.util.ResourceBundle;
-import java.util.UUID;
 
 
 /**
@@ -34,6 +27,7 @@ import java.util.UUID;
  */
 public class RegisterController implements Initializable {
 
+    private Main main;
     @FXML
     private ImageView shieldImageView;
     @FXML
@@ -55,24 +49,52 @@ public class RegisterController implements Initializable {
     @FXML
     private PasswordField confirmPasswordField;
 
-    private Person person = new Person();
+    private Person person;
 
-
-
-    @FXML
-    public void backButtonOnAction(ActionEvent event) throws Exception { // the close button and its action
-        Main main = new Main();
-        main.start(Main.getStage());
+    /**
+     * a method to return Main object which declared in current Class.
+     * @return MainClass Value
+     */
+    public Main getMain() {
+        return main;
     }
 
+    /**
+     * A method to set a MainClass Object into MainClass object which declared in current Class.
+     * @param main
+     */
+    public void setMain(Main main) {
+        this.main = main;
+    }
+
+    /**
+     * A method which includes the functions for the back button.
+     * When we click on back button the stage in current class will go back to start stage.
+     * @param event
+     * @throws Exception
+     */
+    @FXML
+    public void backButtonOnAction(ActionEvent event) throws Exception {
+
+        main.start(main.getStage());
+    }
+
+    /**
+     *A method which includes the functions for the register button.
+     * when we click on register button it will set the person information into database
+     * with help of method registerUser() and will trow the person Object into ShoeTableView in the Main Class.
+     *It will also check if the password and confirmed password are equals.
+     * @param event
+     */
     @FXML
     public void registerButtonOnAction(ActionEvent event) {
         if (setPasswordField.getText().equals(confirmPasswordField.getText())) {
             registerUser();
             confirmedPasswordLabel.setText("Password is set!");
             registrationMessageLabel.setText("User has been registered successfully!");
-            Main main = new Main();
-            main.shoeTableView();
+            //Main main = new Main();
+            main.shoeTableView(person);//TODO set person object
+
 
         } else {
             confirmedPasswordLabel.setText("Password does not match!");
@@ -80,7 +102,12 @@ public class RegisterController implements Initializable {
 
     }
 
-
+    /**
+     * An Override method initialize() which is and abstract method in interface Initializable that we implement in our Class.
+     * We use this method to shoe our pictures in FX.
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         File shieldImageFile = new File("Images/shieldShoeLogo.jpg");
@@ -88,8 +115,13 @@ public class RegisterController implements Initializable {
         shieldImageView.setImage(shieldImage);
     }
 
-    public void registerUser(){
-        person.setId(1);
+    /**
+     * A Method that get information from all TextFiled boxes in the register stage
+     * and add this information to person Object
+     * and insert person to the database with PersonDAO object and method insertRegisteredPerson(Person).
+     */
+    public void registerUser() {
+        person.setId(0);
         person.setName(firstNameTextField.getText());
         person.setFamily(lastNameTextField.getText());
         person.setUserName(usernameTextField.getText());
@@ -101,10 +133,18 @@ public class RegisterController implements Initializable {
         personDAO.insertRegisteredPerson(person);
     }
 
+    /**
+     * a method to return the Person Object which declared in current Class
+     * @return Person Object
+     */
     public Person getPerson() {
         return person;
     }
 
+    /**
+     * A method to set a person Object into the person object which declared in this Class.
+     * @param person
+     */
     public void setPerson(Person person) {
         this.person = person;
     }
